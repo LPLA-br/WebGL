85ca09be0c0e4b7a3e640bde87bf09f4d28e91f4
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2024-07-09 22:03:26 -0300

    adiciona capacidade de alterar texturas das duas esferas em   astronautic

--- src/astronautic/main.js
+++ src/astronautic/main.js
@@ -8,20 +8,44 @@ const scene = new THREE.Scene();
 //A câmera é a espaçonave
 const camera =  new Camera( 0.1, 0.1 , 0.1, 0, 0, 0, 0, 0, 25, new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ));
 new Controle( camera ).executar();
-camera.definirPosicao(0,0,10);
+camera.definirPosicao(0,0,20);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const camada01 = novaEsfera( 10, './texturas/terra.jpg', 1, scene );
-const camada02 = novaEsfera( 10.1, './texturas/terraAtmosfera.jpg', 0.5, scene );
+const caminho01 = document.querySelector("#t1");
+const caminho02 = document.querySelector("#t2");
+const aplicar = document.querySelector("#aplicar");
+const limpar = document.querySelector("#limpar");
+
+let objetos = [];
+
+objetos.push( novaEsfera( 10, './texturas/terra.jpg', 1, scene ) );
+objetos.push( novaEsfera( 10.1, './texturas/terraAtmosfera.jpg', 0.5, scene ) );
+
+aplicar.addEventListener("click", ()=>
+{
+  if ( objetos.length == 2 ) return;
+  objetos.push( novaEsfera( 10, './texturas/' + caminho01.value, 1, scene ) );
+  objetos.push( novaEsfera( 10.1, './texturas/' + caminho02.value, 0.5, scene ) );
+  scene.add( objetos[0] );
+  scene.add( objetos[1] );
+},false);
+
+limpar.addEventListener("click", ()=>
+{
+  for ( let i = 0; i < objetos.length; i++ )
+  {
+    scene.remove( objetos.pop() );
+  }
+},false);
 
 // https://threejs.org/docs/?q=light#api/en/lights/shadows/PointLightShadow
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
 scene.add( directionalLight );
 directionalLight.position.set( - 1, 0, 1 ).normalize();
-directionalLight.target = camada01;
+directionalLight.target = objetos[0];
 
 
 // ambiente
@@ -30,8 +54,13 @@ function animate()
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera.getCamera() );
   camera.moverCameraConformeVelocidade();
-  camada01.rotateY( 0.001 );
-  camada02.rotateY( 0.001 );
+  if ( objetos.length > 0 )
+  {
+    for (let i = 0; i < objetos.length; i++)
+    {
+      objetos[i].rotateY( 0.001 );
+    }
+  }
 }
 
 animate();
