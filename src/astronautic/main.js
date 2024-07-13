683ae1ba75d87cb13134b6caa02304f09005b8c2
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2024-07-13 11:27:52 -0300

    melhora os controles do lpsphere (antigo astronautic)

--- src/astronautic/main.js
+++ src/astronautic/main.js
@@ -1,38 +1,94 @@
 import * as THREE from 'three';
 import { novaEsfera } from './esferas';
-import { Camera } from './camera';
-import { Controle } from './controle';
+//import { Camera } from './camera';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { PerspectiveCamera } from 'three';
 
 const scene = new THREE.Scene();
 
-//A câmera é a espaçonave
-const camera =  new Camera( 0.1, 0.1 , 0.1, 0, 0, 0, 0, 0, 25, new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ));
-new Controle( camera ).executar();
-camera.definirPosicao(0,0,20);
+const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+
+const controls = new OrbitControls( camera, renderer.domElement );
 document.body.appendChild( renderer.domElement );
 
 const caminho01 = document.querySelector("#t1");
 const caminho02 = document.querySelector("#t2");
 const aplicar = document.querySelector("#aplicar");
 const limpar = document.querySelector("#limpar");
+const opacidade01 = document.querySelector("#opacidade01");
+const opacidade02 = document.querySelector("#opacidade02");
+const rotacionar = document.querySelector("#rotacao");
+const luz = document.querySelector("#luz");
 
 let objetos = [];
+let opacidades = {
+  camada01: 1,
+  camada02: 0.5
+};
+let rotacao = true;
+let tipoIluminacao = "direcional";
+let iluminacao = new THREE.DirectionalLight( 0xffffff, 2 );
 
-objetos.push( novaEsfera( 10, './texturas/terra.jpg', 1, scene ) );
-objetos.push( novaEsfera( 10.1, './texturas/terraAtmosfera.jpg', 0.5, scene ) );
+// ----------------------------------------------------------------------------------
 
+objetos.push( novaEsfera( 10, './texturas/terra.jpg', opacidades.camada01, scene ) );
+objetos.push( novaEsfera( 10.1, './texturas/atmosfera.jpg', opacidades.camada02, scene ) );
+
+// pontapé inicial na iluminação para tudo não ficar escuro.
+iluminacao.target = objetos[0];
+iluminacao.position.set( - 1, 0, 1 ).normalize();
+scene.add( iluminacao );
+
+function atualizarOpacidades()
+{
+  opacidades.camada01 = opacidade01.value;
+  opacidades.camada02 = opacidade02.value;
+}
+
+function atualizarRotacao()
+{
+  rotacao = rotacionar.checked;
+}
+
+//alterarTipoLuz
+luz.addEventListener("click", ()=>
+{
+  if ( tipoIluminacao == "omnidirecional" ) tipoIluminacao = "direcional";
+  else if ( tipoIluminacao == "direcional" ) tipoIluminacao = "omnidirecional";
+
+  if ( tipoIluminacao == "omnidirecional" )
+  {
+    scene.remove( iluminacao );
+    iluminacao.dispose();
+    iluminacao = new THREE.AmbientLight( 0xffffff, 2 );
+    scene.add(iluminacao);
+  }
+  else
+  {
+    scene.remove(iluminacao);
+    iluminacao = new THREE.DirectionalLight( 0xffffff, 2 );
+    scene.add( iluminacao );
+    iluminacao.position.set( - 1, 0, 1 ).normalize();
+    iluminacao.target = objetos[0];
+  }
+    
+}, false);
+
+// removerObjetosAntigosEAdicionarNovos
 aplicar.addEventListener("click", ()=>
 {
+  atualizarOpacidades();
   if ( objetos.length == 2 ) return;
-  objetos.push( novaEsfera( 10, './texturas/' + caminho01.value, 1, scene ) );
-  objetos.push( novaEsfera( 10.1, './texturas/' + caminho02.value, 0.5, scene ) );
+  objetos.push( novaEsfera( 10, caminho01.value, opacidades.camada01, scene ) );
+  objetos.push( novaEsfera( 10.1, caminho02.value, opacidades.camada02, scene ) );
   scene.add( objetos[0] );
   scene.add( objetos[1] );
 },false);
 
+//removerCamada
 limpar.addEventListener("click", ()=>
 {
   for ( let i = 0; i < objetos.length; i++ )
@@ -41,26 +97,25 @@ limpar.addEventListener("click", ()=>
   }
 },false);
 
-// https://threejs.org/docs/?q=light#api/en/lights/shadows/PointLightShadow
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 2 );
-scene.add( directionalLight );
-directionalLight.position.set( - 1, 0, 1 ).normalize();
-directionalLight.target = objetos[0];
 
+camera.position.set( 0, 100, 0 );
+controls.update();
 
 // ambiente
 function animate()
 {
 	requestAnimationFrame( animate );
-	renderer.render( scene, camera.getCamera() );
-  camera.moverCameraConformeVelocidade();
-  if ( objetos.length > 0 )
+	renderer.render( scene, camera );
+
+  if ( objetos.length > 0 && rotacao == true)
   {
     for (let i = 0; i < objetos.length; i++)
     {
       objetos[i].rotateY( 0.001 );
     }
   }
+  controls.update();
+  atualizarRotacao();
 }
 
 animate();
