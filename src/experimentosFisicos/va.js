23022b15589d7e0a575ac32f8663cc1626cb652f
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2024-12-26 11:04:52 -0300

    Computa propriedades trigonometricas e as exibe em canvas.

--- src/experimentosFisicos/va.js
+++ src/experimentosFisicos/va.js
@@ -1,4 +1,31 @@
 
+function distancia( objetoA, objetoB )
+{
+  return Math.sqrt( (objetoA.posicaoX - objetoB.posicaoX)**2 + (objetoA.posicaoY - objetoB.posicaoY)**2 );
+}
+
+function seno( objetoA, objetoB, hipotenusa=1 )
+{
+  let oposto = 0;
+
+  if ( objetoA.posicaoY > objetoB.posicaoY )
+  {
+    oposto = objetoA.posicaoY - objetoB.posicaoY;
+  }
+  else
+  {
+    oposto = objetoB.posicaoY - objetoA.posicaoY;
+  }
+  return oposto/hipotenusa;
+}
+
+function correcaoQuadrante2( graus )
+{
+  if ( graus >= 90 || graus <= 180 )
+    return Math.acos(Math.cos(graus))*180/Math.PI;
+  return graus
+}
+
 class Corpo
 {
   constructor( alt, lar, px, py, vx, vy, ax, ay, m )
@@ -56,9 +83,16 @@ async function aoBaixarDeUmaTecla( event )
   {
     o.aceleracaoX -= 0.01;
   }
+  else if ( keyCode == 81 ) //q
+  {
+    o.velocidadeX = 0;
+    o.velocidadeY = 0;
+    o.aceleracaoX = 0;
+    o.aceleracaoY = 0;
+  }
 };
 
-//loop
+let d, s, arcsen, arcos;
 function desenhar()
 {
   if (canvas.getContext)
@@ -91,7 +125,15 @@ function desenhar()
     o.aceleracaoX;
     o.aceleracaoY;
 
+    d = distancia(o,planeta);
+    s = seno(o,planeta,d);
+    arcsen = (Math.asin(seno(o,planeta,distancia(o,planeta)))*180)/Math.PI;
+    arcos = (Math.acos(Math.cos(arcsen))*180)/Math.PI;
     ctx.fillText(`${JSON.stringify(o)}`,10,10);
+    ctx.fillText(`d=${d}`,10,20);
+    ctx.fillText(`sen=${s}`,10,30);
+    ctx.fillText(`arcsen=${arcsen}`,10,40);
+    ctx.fillText(`arcos=${arcos}`,10,50);
     
   }
   window.requestAnimationFrame( desenhar );
